a1805dbef892606e02174efb4e533d171bd86a91
huntermahmud <mahmudthedatahunter@gmail.com>
2025-07-28 23:45:02 +0600

    [solved:golang] codewars 2.vowel count.go

---
+++ CodeWars/Golang/2. Vowel Count.go
@@ -0,0 +1,10 @@
+package kata
+
+func GetCount(str string) (count int) {
+	for i := 0; i < len(str); i++ {
+		if str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' {
+			count++
+		}
+	}
+	return count
+}
